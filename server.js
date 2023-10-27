21f801eb2dd0377d3955d710ec5a5e76c7b9edc2
Shattik Bandyopadhyaa <codecstasy.shattik@gmail.com>
2023-10-27 11:11:34 +0600

    More routing added

--- server.js
+++ server.js
@@ -15,12 +15,25 @@ db.connect((err) => {
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
-app.use('/css', express.static(__dirname + '/public/css/'));
-app.use('/images', express.static(__dirname + '/public/images/'));
 // app.set('view engine', 'ejs');
+app.use(express.static(__dirname, + '/public/script.js'))
+app.use("/public/css", express.static(__dirname + '/public/css/'));
+app.use("/public/images", express.static(__dirname + '/public/images/'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/html/index.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname+ '/public/html/login.html');
+});
+
+app.get('/registration', (req, res) => {
+    res.sendFile(__dirname + '/public/html/registration.html');
+});
+
+app.get('/book', (req, res) => {
+    res.sendFile(__dirname + '/public/html/book.html');
 });
 
 app.listen(port, () => {
