6d78e9c24cb70f4fe405083f8815ae7dffd6abf8
Shattik Bandyopadhyaa <codecstasy.shattik@gmail.com>
2023-10-31 13:11:17 +0600

    Room availability query working, some routing problems fixed

--- server.js
+++ server.js
@@ -25,17 +25,53 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname+ '/public/html/login.html');
+    console.log("Helo login");
+    res.sendFile(__dirname + '/public/html/login.html');
 });
 
 app.get('/registration', (req, res) => {
+    console.log("Helo registration");
     res.sendFile(__dirname + '/public/html/registration.html');
 });
 
-app.get('/book', (req, res) => {
-    res.sendFile(__dirname + '/public/html/book.html');
+app.get('/room_availability', (req, res) => {
+    console.log("Hello room availability");
+    const available = [];
+    const queryCount = 6; // The number of queries to run
+    let completedQueries = 0; // Initialize the counter
+
+    const handleQueryResult = (err, result) => {
+        if (err) {
+            console.error('Error fetching available room count: ', err);
+            return 0;
+        }
+        const availableRoomCount = result[0].roomCount;
+        available.push(availableRoomCount);
+        completedQueries++;
+
+        if (completedQueries === queryCount) {
+            // All queries have completed, send the response
+            console.log(available);
+            res.sendFile(__dirname + '/public/html/room_availability.html');
+        }
+    };
+
+    const queries = [
+        'SELECT COUNT(*) AS roomCount FROM room WHERE is_available_now = 1 AND room_type_id = 1',
+        'SELECT COUNT(*) AS roomCount FROM room WHERE is_available_now = 1 AND room_type_id = 2',
+        'SELECT COUNT(*) AS roomCount FROM room WHERE is_available_now = 1 AND room_type_id = 3',
+        'SELECT COUNT(*) AS roomCount FROM room WHERE is_available_now = 1 AND room_type_id = 4',
+        'SELECT COUNT(*) AS roomCount FROM room WHERE is_available_now = 1 AND room_type_id = 5',
+        'SELECT COUNT(*) AS roomCount FROM room WHERE is_available_now = 1 AND room_type_id = 6'
+    ];
+
+    queries.forEach((query) => {
+        db.query(query, (err, results) => {
+            handleQueryResult(err, results);
+        });
+    });
 });
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
-})
+});
