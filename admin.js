38c9c06d5ebe0c33b28ca416df9ec44ea21881b3
Shattik Bandyopadhyaa <codecstasy.shattik@gmail.com>
2023-11-15 14:55:15 +0600

    Admin's confirmation successful

--- admin.js
+++ admin.js
@@ -30,7 +30,47 @@ app.get('/', (req, res) => {
 
 app.get('/bookings', (req, res) => {
     console.log("Hello Admin Bookings");
-    res.render('Admin_panel');
+    // res.render('Admin_panel');
+    const sql = `SELECT * FROM reservation`;
+
+    db.query(sql, (err, result) => {
+        if(err) {
+            console.log('Error fetching booking data from the database');
+            res.redirect('/');
+        } else {
+            console.log('Successfully fetched the booking data');
+            res.render('Admin_Panel', { rooms: result });
+        }
+    });
+});
+
+app.post('/updateBookings', (req, res) => {
+    const { approved, rejected } = req.body;
+
+    // Update the database with the approved and rejected bookings
+    const updateQuery = `
+        UPDATE reservation
+        SET reservation_status = CASE
+            WHEN reservation_id IN (${approved.join(',')}) THEN 'approved'
+            WHEN reservation_id IN (${rejected.join(',')}) THEN 'rejected'
+            ELSE reservation_status
+        END;
+    `;
+
+    db.query(updateQuery, (err, result) => {
+        if (err) {
+            console.error('Error updating bookings:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            console.log('Bookings updated successfully');
+            res.json({ message: 'Bookings updated successfully' });
+        }
+    });
+});
+
+app.get('/confirm_decision', (req, res) => {
+    console.log("Hello confirm decision");
+    res.redirect('/');
 });
 
 app.get('/rooms', (req, res) => {
