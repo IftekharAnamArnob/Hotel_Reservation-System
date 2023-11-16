4a0cbf48f8e8f99d193ff630a350bbe8606818ab
Shattik Bandyopadhyaa <codecstasy.shattik@gmail.com>
2023-11-16 20:15:55 +0600

    Admin authentication and registration system added

--- admin.js
+++ admin.js
@@ -24,6 +24,65 @@ app.use("/public/css", express.static(__dirname + '/public/css/'));
 app.use("/public/images", express.static(__dirname + '/public/images/'));
 
 app.get('/', (req, res) => {
+    console.log('Hello Admin');
+    res.render('admin_login');
+});
+
+app.post('/admin_login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    console.log(`Email: ${email}, Password: ${password}`);
+
+    authenticateUser(email, password, (err, user) => {
+        if(err) {
+            console.log("Authentication error");
+            res.redirect('/');
+        } else {
+            console.log("Admin authenticated");
+            res.redirect('/home');
+        }
+    })
+});
+
+function authenticateUser(email, password, callback) {
+    const sql = 'SELECT * FROM admin WHERE email = ? AND user_password = ?';
+    db.query(sql, [email, password], (err, results) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      if (results.length > 0) {
+        // User authenticated successfully
+        callback(null, results[0]);
+      } else {
+        // User not found or invalid credentials
+        callback('Invalid credentials', null);
+      }
+    });
+};
+
+app.get('/admin_registration', (req, res) => {
+    console.log('Hello admin registration');
+    res.render('admin_registration');
+});
+
+app.post('/submit_registration', (req, res) => {
+    const { firstName, lastName, admin_type, email, phone, password } = req.body;
+
+    const insertQuery = 'INSERT INTO admin (first_name, last_name, admin_type, email, phone, user_password) VALUES (?, ?, ?, ?, ?, ?)';
+    db.query(insertQuery, [firstName, lastName, admin_type, email, phone, password], (err, result) => {
+        if (err) {
+            console.error('Error inserting admin registration:', err);
+            res.redirect('/admin_registration');
+        } else {
+            console.log('Admin Registration successful');
+            res.redirect('/home');
+        }
+    });
+});
+
+app.get('/home', (req, res) => {
     console.log("Hello Admin Home");
     res.render('Admin_Home');
 });
@@ -36,7 +95,7 @@ app.get('/bookings', (req, res) => {
     db.query(sql, (err, result) => {
         if(err) {
             console.log('Error fetching booking data from the database');
-            res.redirect('/');
+            res.redirect('/home');
         } else {
             console.log('Successfully fetched the booking data');
             res.render('Admin_Panel', { rooms: result });
@@ -80,7 +139,7 @@ app.post('/updateBookings', (req, res) => {
 
 app.get('/confirm_decision', (req, res) => {
     console.log("Hello confirm decision");
-    res.redirect('/');
+    res.redirect('/home');
 });
 
 app.get('/room_prices', (req, res) => {
